3e37e45feb0846cf773d3c516f562959ee42abcd
Ricardo Jorge <ricardojorgexyz@gmail.com>
2026-06-20 14:21:48 +0100

    fix: errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
